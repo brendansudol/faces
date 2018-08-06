1d8c8d896462cbfebe55d8e87de8b9b4c65da431
brendan sudol <brendansudol@gmail.com>
2018-08-06 09:29:45 -0400

    detecting faces

--- src/App2.js
+++ src/App2.js
@@ -3,21 +3,43 @@ import * as faceapi from 'face-api.js/dist/face-api.js'
 
 import img from './img/faces.jpg'
 
+const getImg = src =>
+  new Promise(resolve => {
+    const img = new Image()
+    img.src = src
+    img.crossOrigin = '*'
+    img.onload = () => resolve(img)
+  })
+
 class App2 extends Component {
   componentDidMount() {
-    this.go()
+    this.initModel()
   }
 
-  go = async () => {
-    console.log(faceapi)
-    // await faceapi.loadFaceDetectionModel('/models')
+  initModel = async () => {
+    const model = new faceapi.FaceDetectionNet()
+    const path =
+      `${process.env.PUBLIC_URL}/static/model/face/` +
+      'ssd_mobilenetv1_model-weights_manifest.json'
+
+    await model.load(path)
+
+    const myImg = await getImg(img)
+    const detections = await model.locateFaces(myImg, 0.4, 50)
+    console.log(detections)
+
+    this.canvas.width = myImg.width
+    this.canvas.height = myImg.height
+    faceapi.drawDetection(this.canvas, detections, { withScore: true })
   }
 
   render() {
     return (
       <div className="container">
-        <img src={img} alt="demo" />
-        <canvas ref={el => (this.canvas = el)} />
+        <div className="relative">
+          <img src={img} alt="demo" />
+          <canvas ref={el => (this.canvas = el)} className="overlay" />
+        </div>
       </div>
     )
   }
