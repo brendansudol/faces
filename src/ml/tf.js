c52fb655b9e30a0504020e2a19510902c06d372d
brendan sudol <brendansudol@gmail.com>
2018-08-29 16:33:27 -0400

    remove unneeded lib

--- src/ml/tf.js
+++ src/ml/tf.js
@@ -1,5 +1,4 @@
 import * as core from '@tensorflow/tfjs-core'
-import * as converter from '@tensorflow/tfjs-converter'
 import * as layers from '@tensorflow/tfjs-layers'
 
-export const tf = { ...core, ...converter, ...layers }
+export const tf = { ...core, ...layers }
